dd8e1154f9b6713ed8acbac74fd761cedf2bd999
IsadoraPSouza <isadora.souza@estudante.ifgoiano.edu.br>
2022-10-20 17:04:24 -0300

    página de ajustes

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Text } from 'react-native';
-import { Container } from './styles';
 
-export default () => {
-    return (
-        <Container>
-            <Text>Ajustes</Text>
-        </Container>
-    );
-}
+import {Text, View} from 'react-native';
+import SettingsComponent from "../../components/SettingsComponent";
+
+const Settings = () =>  {
+    <Text>Configurações</Text>
+    const settingsOptions = [
+        {title:'Endereço de entrega', subTitle: null, onPress: () => {}},
+        {title:'Dados Pessoais', subTitle: null, onPress: () => {}},
+        {title:'Alterar Senha', subTitle: null, onPress: () => {}},
+        {title:'Desativar minha conta', subTitle: null, onPress: () => {}},
+        {title:'Sobre OFairta', subTitle: null, onPress: () => {}},
+        {title:'Preciso de ajuda?', subTitle: null, onPress: () => {}},
+    ];
+
+    return <SettingsComponent settingsOptions={settingsOptions}/>;
+};
+
+export default Settings;
